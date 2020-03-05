aacae8c931d228f72ae3f0d58a233c66fa4b81fc
Hau Soft <50167033+HiSoTeam@users.noreply.github.com>
2020-03-05 20:46:15 +0700

    Update load Html
    Update load Html

--- AZERP/wwwroot/lib/az-core/js/az-url.js
+++ AZERP/wwwroot/lib/az-core/js/az-url.js
@@ -1,18 +1,18 @@
 ﻿function AZUrl() { }
-AZUrl.prototype.changeUrl = function (url) {
+AZUrl.prototype.loadHtml = function (url) {
     new AZAjax().DoGet(url, {}, function (d) { $("#ContentAZ").html(d.Html); }, function (e) { });
-   
+}
+AZUrl.prototype.changeUrl = function (url) {
+    this.loadHtml(url);
+    window.history.pushState("data", "Title", url);
 }
 AZUrl.prototype.Init = function () {
     var $this = this;
     $("a.az-link").on("click", function (e) {
         e.preventDefault();
-        var url = $(this).attr("href");
-        window.history.pushState("data", "Title", url);
-        $this.changeUrl(url);
+        $this.changeUrl($(this).attr("href"));
     });
     $(window).on('popstate', function (e) {
-        var url = location.pathname + location.search;
-        $this.changeUrl(url);        
+        $this.loadHtml(location.pathname + location.search);        
     });
 }
