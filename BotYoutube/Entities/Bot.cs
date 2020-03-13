bcfb0159bcc2021d967d8bcb16dc6f54833a4c6e
Hau Soft <50167033+HiSoTeam@users.noreply.github.com>
2020-03-13 22:26:24 +0700

    ok
    ok

---
+++ BotYoutube/Entities/Bot.cs
@@ -0,0 +1,31 @@
+﻿using BotYoutube.Database;
+using BotYoutube.Database.Attr;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotYoutube.Entities
+{
+    [TableInfo(TableName = "bot_bot")]
+    public class BotModel : EntityModel<BotModel, long>
+    {
+        [Field]
+        public string ip { get; set; }
+        [Field]
+        public DateTime LastLogin { get; set; }
+        [Field]
+        public DateTime LastProcess { get; set; }
+        [Field]
+        public bool IsActice { get; set; }
+
+    }
+    public class BotService : EntityService<BotService, BotModel>
+    {
+        public BotService(IDbConnection _connection) : base(_connection)
+        {
+        }
+    }
+}
---
+++ BotYoutube/Manager/Bot/UpdateBot.cs
@@ -0,0 +1,24 @@
+﻿using BotYoutube.Database;
+using BotYoutube.Entities;
+using BotYoutube.UI;
+
+namespace BotYoutube.Manager.Bot
+{
+    public partial class UpdateBot : FormUpdate
+    {
+        public UpdateBot()
+        {
+            InitializeComponent();
+        }
+        public override IEntityModel GetDataNew()
+        {
+            return new LinkModel();
+        }
+        public override void AfterLoad()
+        {
+            if (this.Model != null) {
+                //txtUser.Enabled = false;
+            }
+        }
+    }
+}
