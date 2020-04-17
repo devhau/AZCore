f474bc495628d8061ae3c1998e63fc9bfe6d7cfd
zoldkyllua <zold.ithunter@gmail.com>
2020-04-17 20:08:50 +0700

    Create Customers

---
+++ AZERP/Web/Modules/Customers/FormCustomers.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using AZCore.Database.Enums;
+using AZERP.Data.Entities;
+using AZERP.Data.Enums;
+using AZWeb.Module.Attributes;
+using AZWeb.Module.Enums;
+using AZWeb.Module.Page.Manager;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Drawing;
+
+namespace AZERP.Web.Modules.Customers
+{
+    [TableColumn(Title = "Mã khách hàng", FieldName = "Code")]
+    [TableColumn(Title = "Tên khách hàng", FieldName = "FullName")]
+    [TableColumn(Title = "Giới tính", FieldName = "Gender")]
+    [TableColumn(Title = "Email", FieldName = "Email")]
+    [TableColumn(Title = "Số điện thoại", FieldName = "PhoneNumber")]
+    [TableColumn(Title = "Trạng thái", FieldName = "CustomersStatus", DataType = typeof(CustomersStatus))]
+    public class FormCustomers : ManageModule<CustomersService, CustomersModel, FormUpdateCustomers>
+    {
+        #region -- Field Search --
+        /// <summary>
+        /// Họ Tên
+        /// </summary>
+        [QuerySearch(OperatorSQL = OperatorSQL.LIKE)]
+        public string FullName { get; set; }
+        /// <summary>
+        /// Giới tính
+        /// </summary>
+        [QuerySearch]
+        public Gender? Gender { get; set; }
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        [QuerySearch(OperatorSQL = OperatorSQL.LIKE)]
+        public string PhoneNumber { get; set; }
+        #endregion
+
+        public FormCustomers(IHttpContextAccessor httpContext) : base(httpContext)
+        {
+        }
+        protected override void IntData()
+        {
+            this.Title = "Khách hàng";
+        }
+    }
+}
---
+++ AZERP/Web/Modules/Product/PurchaseOrders/js/PurchaseOrders.js
@@ -0,0 +1,14 @@
+﻿function calMoney(number, price) {
+    if (isNaN(number) && isNaN(price)) {
+        return 0;
+    }
+    return (parseInt(number) * parseInt(price)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+$("#InputImportNumber").on('input', function () {
+    $("#LabelThanhTien").text(calMoney($("#InputImportNumber").val(), $("#InputImportPrice").val()));
+});
+
+$("#InputImportPrice").on('input', function () {
+    $("#LabelThanhTien").text(calMoney($("#InputImportNumber").val(), $("#InputImportPrice").val()));
+});
