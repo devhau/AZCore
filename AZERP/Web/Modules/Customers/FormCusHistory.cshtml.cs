99bfd5bced4afe0f4ca17ea3a25c61b7e58dac71
zoldkyllua <zold.ithunter@gmail.com>
2020-04-18 21:38:40 +0700

    commit

---
+++ AZERP/Web/Modules/Customers/FormCusHistory.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using AZCore.Database.Enums;
+using AZERP.Data.Entities;
+using AZERP.Data.Enums;
+using AZWeb.Module.Attributes;
+using AZWeb.Module.Enums;
+using AZWeb.Module.Page.Manager;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Drawing;
+
+namespace AZERP.Web.Modules.Customers
+{
+    public class FormCusHistory : ManageModule<CustomersService, CustomersModel>
+    {
+
+        public FormCusHistory(IHttpContextAccessor httpContext) : base(httpContext)
+        {
+        }
+        protected override void IntData()
+        {
+            this.Title = "Lịch sử mua hàng";
+        }
+    }
+}
