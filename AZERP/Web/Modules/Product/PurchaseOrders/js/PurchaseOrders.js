8d2892ea10412fcab77756b34e36dd32d8bf8735
zoldkyllua <zold.ithunter@gmail.com>
2020-04-18 11:23:58 +0700

    update nhap hang

--- AZERP/Web/Modules/Product/PurchaseOrders/js/PurchaseOrders.js
+++ AZERP/Web/Modules/Product/PurchaseOrders/js/PurchaseOrders.js
@@ -1,14 +1,26 @@
-﻿function calMoney(number, price) {
+﻿$(".modal-dialog .az-data-table").hide();
+function calMoney(number, price) {
     if (isNaN(number) && isNaN(price)) {
         return 0;
     }
     return (parseInt(number) * parseInt(price)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
-$("#InputImportNumber").on('input', function () {
-    $("#LabelThanhTien").text(calMoney($("#InputImportNumber").val(), $("#InputImportPrice").val()));
+$("#InputProductCode").on('change', function () {
+    let value = $("#InputProductCode").val();
+    if (value != "") {
+        $(".modal-dialog .az-data-table").show();
+        let name = $("#InputProductCode option[value=" + value + "]").text();
+        let inputNumber = '<input type="text" class="form-control" value="1" name="ImportNumber">';
+        let inputPrice = '<input type="text" class="form-control" value="100000" name="inputPrice">';
+        $(".modal-dialog .az-data-table table > tbody:last-child").append("<tr><td></td><td>" + name + "</td><td>" + inputNumber + "</td><td>" + inputPrice + "</td><td><label class='LabelThanhTien'>100,000</label></td></tr>");
+        $(".modal-dialog .az-data-table table input").on('input', function () {
+            console.log($(this).parent().parent().children("td").eq(2).children("input").val());
+
+            let number = $(this).parent().parent().children("td").eq(2).children("input").val();
+            let price = $(this).parent().parent().children("td").eq(3).children("input").val();
+            $(this).parent().parent().children("td:last-child").children("label").text(calMoney(number, price));
+        });
+    }
 });
 
-$("#InputImportPrice").on('input', function () {
-    $("#LabelThanhTien").text(calMoney($("#InputImportNumber").val(), $("#InputImportPrice").val()));
-});
