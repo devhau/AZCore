a9c4f76645f269d77f3b869bf881fc3b642400c2
cuipro92 <cuipro92@gmail.com>
2020-04-19 23:53:59 +0700

    Commit FormArea in Hotel
    Commit Form Area

---
+++ AZERP.Data/Entities/Hotel/TypeOfHotel.cs
@@ -0,0 +1,41 @@
+﻿using AZCore.Database;
+using AZCore.Database.Attributes;
+using AZCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AZERP.Data.Entities.Hotel
+{
+    public class TypeOfHotelService : EntityService<TypeOfHotelService, TypeOfHotelModel>, IAZTransient
+    {
+        public TypeOfHotelService(IDbConnection _connection) : base(_connection)
+        {
+        }
+    }
+    /// <summary>
+    /// Thông tin các loại của nhà trọ
+    /// </summary>
+
+    [TableInfo(TableName = "az_hotel_typeofhotel")]
+    public class TypeOfHotelModel : EntityModel<TypeOfHotelModel, long>
+    {
+        /// <summary>
+        /// Mã loại phòng trọ
+        /// </summary>
+        [Field(Length = 500)]
+        public string TypeOfHotelID { get; set; }
+        /// <summary>
+        /// Tên loại phòng trọ
+        /// Ví dụ : 15m2, 25m2, 50m2, 100m2
+        /// </summary>
+        [Field(Length = 500)]
+        public string TypeOfHotelName { get; set; }
+        /// <summary>
+        /// Ghi chú
+        /// </summary>
+        [Field]
+        public string Note { get; set; }
+    }
+}
---
+++ AZERP.Data/Enums/HotelStatus.cs
@@ -0,0 +1,15 @@
+﻿using AZCore.Database.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AZERP.Data.Enums
+{
+    public enum HotelStatus:int
+    {
+        [Field(Display = "Chưa hoạt động")] None = 0,//Default
+        [Field(Display = "Đang hoạt động: chưa có người sử dụng")] Active_NonUsed = 1,
+        [Field(Display = "Đang hoạt động: có người sử dụng")] Active_Used = 2,
+        [Field(Display = "Khác")] Other = 3,
+    }
+}
