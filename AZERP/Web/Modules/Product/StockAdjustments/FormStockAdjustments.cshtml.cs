6cb3ad3094f720343f6eb499f74c5f093e3d30f0
zoldkyllua <zold.ithunter@gmail.com>
2020-04-20 18:22:36 +0700

    merge

--- AZERP/Web/Modules/Product/StockAdjustments/FormStockAdjustments.cshtml.cs
+++ AZERP/Web/Modules/Product/StockAdjustments/FormStockAdjustments.cshtml.cs
@@ -6,18 +6,34 @@ using AZWeb.Module.Common;
 using AZWeb.Module.Page.Manager;
 using Microsoft.AspNetCore.Http;
 using System;
+using System.Linq;
 
 namespace AZERP.Web.Modules.Product.StockAdjustments
 {
     [TableColumn(Title = "Mã đơn", FieldName = "Code")]
     public class FormStockAdjustments : ManageModule<StockAdjusmentService, StockAdjusmentModel, FormUpdateStockAdj>
     {
-        public FormStockAdjustments(IHttpContextAccessor httpContext) : base(httpContext)
+        ProductService productService;
+        public FormStockAdjustments(IHttpContextAccessor httpContext, ProductService productService) : base(httpContext)
         {
+            this.productService = productService;
         }
         protected override void IntData()
         {
             this.Title = "Kiểm hàng";
+        }
+        public IView GetProducbyid(long Id)
+        {
+
+            var result = productService.Select(p => p.Id == Id).FirstOrDefault();
+            if (result != null)
+            {
+                return Json("", result);
+            }
+            else
+            {
+                return Json("");
+            }
         }
     }
 }
