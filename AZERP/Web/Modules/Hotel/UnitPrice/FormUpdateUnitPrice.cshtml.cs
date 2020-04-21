084dffce115c2c8bb82e7d3763434b71aa8c8fd7
cuipro92 <cuipro92@gmail.com>
2020-04-22 00:23:49 +0700

    commit don gia
    commit don gia

---
+++ AZERP/Web/Modules/Hotel/UnitPrice/FormUpdateUnitPrice.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using AZERP.Data.Entities;
+using AZERP.Data.Entities.Hotel;
+using AZWeb.Module.Common;
+using AZWeb.Module.Page.Manager;
+using Microsoft.AspNetCore.Http;
+
+namespace AZERP.Web.Modules.Hotel.UnitPrice
+{
+    public class FormUpdateUnitPrice : UpdateModule<UnitPriceService, UnitPriceModel>
+    {
+        public FormUpdateUnitPrice(IHttpContextAccessor httpContext) : base(httpContext)
+        {
+        }
+        protected override void IntData()
+        {
+            this.Title = "Đơn giá";
+        }
+
+    }
+}
