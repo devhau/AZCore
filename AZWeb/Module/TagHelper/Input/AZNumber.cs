0a82b381636116a8c5b8bbbd8bba0097370dfee7
zoldkyllua <zold.ithunter@gmail.com>
2020-04-22 17:39:45 +0700

    merge code

--- AZWeb/Module/TagHelper/Input/AZNumber.cs
+++ AZWeb/Module/TagHelper/Input/AZNumber.cs
@@ -23,23 +23,38 @@ namespace AZWeb.Module.TagHelper.Input
     {
         public NumberType Type { get; set; } = NumberType.Integer;
         public int Digits { get; set; } = 2;
+        public string GroupSeparator { get; set; } = ",";
+        public bool AutoGroup { get; set; } = true;
         protected override void RenderHtml(StringBuilder htmlBuild)
         {
-            this.Attr+= " data-inputmask=\"'alias': '"+ Type.ToString().ToLower()+ "'\" ";
+            //phonebe
+            if (Type == NumberType.PhoneNumber)
+            {
+                Digits = 0;
+                GroupSeparator = "";
+                AutoGroup = false;
+                this.Attr += " data-inputmask=\"'alias': 'phonebe'\" ";
+            }
+            else
+            {
+                this.Attr += " data-inputmask=\"'alias': '" + Type.ToString().ToLower() + "'\" ";
+            }
             if (!string.IsNullOrEmpty(InputLabel))
                 htmlBuild.AppendFormat("<label for=\"{1}\">{0}</label>", InputLabel, InputId);
             htmlBuild.AppendFormat("<input class=\"{1}\" id=\"{2}\" placeholder=\"{3}\" {4} {5} name=\"{6}\">", "number", TagClass, InputId, InputPlaceholder, Attr, InputValue.IsNullOrEmpty() ? "" : string.Format("value =\"{0}\"", InputValue), InputName);
             if (Digits <= 0 || Type == NumberType.Integer) {
                 Digits = 0;
             }
-            this.AddJS("$('."+TagId+ "').inputmask({ rightAlign: false,groupSeparator:',' ,digits:'"+this.Digits+ "',autoGroup: true });");
+            
+            this.AddJS($"$('.{TagId}').inputmask({{ rightAlign: false,groupSeparator:'{GroupSeparator}' ,digits:'{Digits}',autoGroup: {AutoGroup.ToString().ToLower()} }});");
         }
     }
     public enum NumberType
     {
         Integer,
         Decimal,
-        Currency
+        Currency,
+        PhoneNumber
 
     }
 }
