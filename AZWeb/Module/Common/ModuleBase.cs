38553d4de883a6f651fd417d0c2ab10f32e01be5
Hau Soft <50167033+HiSoTeam@users.noreply.github.com>
2020-04-23 16:44:16 +0700

    fix BindService
    fix BindService

--- AZWeb/Module/Common/ModuleBase.cs
+++ AZWeb/Module/Common/ModuleBase.cs
@@ -1,5 +1,8 @@
-﻿using AZWeb.Extensions;
+﻿using AZCore.Extensions;
+using AZWeb.Extensions;
+using AZWeb.Module.Attributes;
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace AZWeb.Module.Common
 {
@@ -9,6 +12,7 @@ namespace AZWeb.Module.Common
         public HttpContext HttpContext { get; }
         public HttpRequest Request { get => HttpContext.Request; }
         public HttpResponse Response { get => HttpContext.Response; }
+        public IServiceProvider RequestServices { get => HttpContext.RequestServices; }
         public bool IsAjax { get; }
         private string path { get; }
         public ModuleBase(IHttpContextAccessor httpContextAccessor)
@@ -21,8 +25,22 @@ namespace AZWeb.Module.Common
             this.HttpContext.BindQueryAttributeTo(this); var pathFull = this.GetType().FullName;
             var indexEnd = pathFull.LastIndexOf('.');
             var indexStart = pathFull.IndexOf(".Web.");
-            path = pathFull.Substring(indexStart + 1, indexEnd - indexStart - 1).Replace(".", "/"); 
-          
+            path = pathFull.Substring(indexStart + 1, indexEnd - indexStart - 1).Replace(".", "/");
+            //BindServiceAttribute
+            foreach (var item in this.GetType().GetProperties())
+            {
+                if (item.GetAttribute<BindServiceAttribute>() != null) 
+                {
+                    item.SetValue(this, RequestServices.GetService(item.PropertyType));
+                }
+            }
+            foreach (var item in this.GetType().GetFields())
+            {
+                if (item.GetAttribute<BindServiceAttribute>() != null &&item.GetValue(this)==null)
+                {
+                    item.SetValue(this, RequestServices.GetService(item.FieldType));
+                }
+            }
         }
         public virtual void BeforeRequest() { IntData(); }
         public virtual void AfterRequest() { }
