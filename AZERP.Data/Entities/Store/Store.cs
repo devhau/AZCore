720794ac07b7d54096a6f9915ce830c0c38480a8
zoldkyllua <zold.ithunter@gmail.com>
2020-04-29 00:59:26 +0700

    Quản lý tồn kho

--- AZERP.Data/Entities/Store/Store.cs
+++ AZERP.Data/Entities/Store/Store.cs
@@ -1,5 +1,6 @@
 ﻿using AZCore.Database;
 using AZCore.Database.Attributes;
+using AZCore.Database.SQL;
 using AZCore.Domain;
 using AZERP.Data.Enums;
 using AZWeb.Module.Attributes;
@@ -15,6 +16,39 @@ namespace AZERP.Data.Entities
         public StoreService(IDbConnection _connection) : base(_connection)
         {
         }
+        public IEnumerable<VariantData> GetObject(long storeId)
+        {
+            var sql = new SQLResult();
+            sql.Param = new Dapper.DynamicParameters();
+            sql.Param.Add("@storeId", storeId);
+            string strWhere = "";
+            if(storeId > 0)
+            {
+                strWhere = " WHERE `az_purchase_order`.StoreId = @storeId ";
+            }
+
+            sql.SQL = string.Format(@"
+                SELECT `az_purchase_order`.StoreId ,`az_purchase_order_product`.ProductId, `az_store_product`.Available , 
+                SUM(CASE WHEN az_purchase_order.Type = 0 AND az_purchase_order.PurchaseOrderImport = 0 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Incoming,
+                SUM(CASE WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 3 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) OnWay,
+                SUM(CASE WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 2 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Committed
+                FROM `az_purchase_order`
+	                LEFT JOIN `az_purchase_order_product` on `az_purchase_order`.Id = `az_purchase_order_product`.PurchaseOrderId
+                    LEFT JOIN `az_store_product`  on `az_purchase_order`.StoreId = `az_store_product`.StoreId AND `az_purchase_order_product`.ProductId = `az_store_product`.ProductId
+                {0} GROUP BY `az_purchase_order`.StoreId ,`az_purchase_order_product`.ProductId
+                ", strWhere);
+            return this.ExecuteQuery<VariantData>(sql);
+        }
+    }
+
+    public class VariantData
+    {
+        public long StoreId { get; set; }
+        public long ProductId { get; set; }
+        public long Available { get; set; }
+        public long Incoming { get; set; }
+        public long OnWay { get; set; }
+        public long Committed { get; set; }
     }
     /// <summary>
     /// Thông tin kho
--- AZERP/Web/Modules/Product/Variants/FormVariants.cshtml.cs
+++ AZERP/Web/Modules/Product/Variants/FormVariants.cshtml.cs
@@ -11,31 +11,59 @@ using System.Linq;
 
 namespace AZERP.Web.Modules.Product.Variants
 {
-    [TableColumn(Title = "Sản phẩm", FieldName = "Name")]
-    [TableColumn(Title = "Mã SKU", FieldName = "Code")]
-    [TableColumn(Title = "Nhóm", FieldName = "CategoryId", DataType = typeof(CategoryService))]
-    [TableColumn(Title = "Tồn kho", FieldName = "Available")]
-    [TableColumn(Title = "Hàng đang về", FieldName = "Incoming")]
-    [TableColumn(Title = "Hàng đang giao", FieldName = "OnWay")]
-    [TableColumn(Title = "Đang giao dịch", FieldName = "Committed")]
-    [TableColumn(Title = "Trạng Thái", FieldName = "ProductSellable", TextFalse = "Đang không hoạt động", TextTrue = "Đang hoạt động")]
+    //[TableColumn(Title = "Mã SKU", FieldName = "Code")]
+    //[TableColumn(Title = "Sản phẩm", FieldName = "Name")]
+    //[TableColumn(Title = "Nhóm", FieldName = "CategoryId", DataType = typeof(CategoryService))]
+    //[TableColumn(Title = "Tồn kho", FieldName = "Available")]
+    //[TableColumn(Title = "Hàng đang về", FieldName = "Incoming")]
+    //[TableColumn(Title = "Hàng đang giao", FieldName = "OnWay")]
+    //[TableColumn(Title = "Đang giao dịch", FieldName = "Committed")]
+    //[TableColumn(Title = "Trạng Thái", FieldName = "ProductSellable", TextFalse = "Đang không hoạt động", TextTrue = "Đang hoạt động")]
     public class FormVariants : ManageModule<ProductService, ProductModel, FormUpdateVariants>
     {
         #region -- Field Search --
+        /// <summary>
+        /// Mã sản phẩm
+        /// </summary>
+        [QuerySearch(OperatorSQL = OperatorSQL.LIKE)]
+        public string Code { get; set; }
         /// <summary>
         /// Tên Sản Phẩm
         /// </summary>
         [QuerySearch(OperatorSQL = OperatorSQL.LIKE)]
         public string Name { get; set; }
         /// <summary>
-        /// Nhóm SP
+        /// Mã kho
         /// </summary>
-        [QuerySearch]
-        public long? CategoryId { get; set; }
+        [BindQuery]
+        public long StoreId { get; set; } = 0;
         #endregion
+
+        [BindService]
+        public StoreService StoreService;
+
+        public List<StoreModel> storeModels;
+        public List<VariantData> variantDatas;
+
+        public long GetInfo(long storeId, long productId, Func<VariantData, long> func)
+        {
+            var item = variantDatas.FirstOrDefault(p => p.StoreId == storeId && p.ProductId == productId);
+            return item == null ? 0 : func(item);
+        }
 
         public override List<ProductModel> GetSearchData()
         {
+            if (this.StoreId == 0)
+            {
+                this.storeModels = StoreService.GetAll().ToList();
+            }
+            else
+            {
+                this.storeModels = StoreService.Select(p => p.Id == this.StoreId).ToList();
+            }
+
+            this.variantDatas = StoreService.GetObject(this.StoreId).ToList();
+
             var proper = this.GetType().GetPropertyByQuerySearch();
             Action<QuerySQL> actionWhere = (T) =>
             {
@@ -44,6 +72,7 @@ namespace AZERP.Web.Modules.Product.Variants
                     if (p.Property.GetValue(this) != null)
                         T.AddWhere(p.Property.Name, p.Property.GetValue(this), p.OperatorSQL);
                 }
+
             };
             this.PageTotalAll = Service.ExecuteNoneQuery((T) => {
 
@@ -61,14 +90,14 @@ namespace AZERP.Web.Modules.Product.Variants
             }
             return Service.ExecuteQuery((T) => {
                 T.Pagination(PageIndex, PageSize);
-                T.Join("az_purchase_order_product", (t1, t2) =>$"{t1}.Id={t2}.ProductId", JoinType.LeftOuterJoin);
-                T.Join("az_purchase_order_product", "az_purchase_order", (t1, t2) => $"{ t1}.PurchaseOrderId={t2}.Id", JoinType.LeftOuterJoin);
-                T.SetColumn(@" az_product.* , 
-                  SUM(CASE  WHEN az_purchase_order.Type = 0 AND az_purchase_order.PurchaseOrderImport = 0 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Incoming,
-                  SUM(CASE  WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 3 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) OnWay,
-                  SUM(CASE  WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 2 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Committed
-                    ");
-                T.AddGroup("az_product.Id");
+                //T.Join("az_purchase_order_product", (t1, t2) =>$"{t1}.Id={t2}.ProductId", JoinType.LeftOuterJoin);
+                //T.Join("az_purchase_order_product", "az_purchase_order", (t1, t2) => $"{ t1}.PurchaseOrderId={t2}.Id", JoinType.LeftOuterJoin);
+                //T.SetColumn(@" az_product.* , 
+                //  SUM(CASE  WHEN az_purchase_order.Type = 0 AND az_purchase_order.PurchaseOrderImport = 0 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Incoming,
+                //  SUM(CASE  WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 3 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) OnWay,
+                //  SUM(CASE  WHEN az_purchase_order.Type = 1 AND az_purchase_order.PurchaseOrderImport = 2 AND az_purchase_order.PurchaseOrderStatus = 0 THEN az_purchase_order_product.ImportNumber ELSE 0 END) Committed
+                //    ");
+                //T.AddGroup("az_product.Id");
                 actionWhere(T);
             }).ToList();
         }
