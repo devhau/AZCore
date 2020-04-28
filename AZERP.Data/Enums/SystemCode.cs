2d6bc196ef93839c98d602edf4ac4f8c56c5cd4a
Hau Soft <50167033+HiSoTeam@users.noreply.github.com>
2020-04-28 10:23:45 +0700

    fix code
    fix code

--- AZERP.Data/Enums/SystemCode.cs
+++ AZERP.Data/Enums/SystemCode.cs
@@ -5,6 +5,12 @@ namespace AZERP.Data.Enums
     public enum SystemCode:int
     {
         [Field(Display = "Khu nhà trọ")]
-        AreaCode
+        AreaCode,
+        [Field(Display ="Ứng viên tuyển dụng")]
+        CandidateCode,
+        [Field(Display = "Công nhân")]
+        WorkerCode,
+        [Field(Display = "Cộng tác viên tuyển dụng")]
+        CollaboratorCode, 
     }
 }
