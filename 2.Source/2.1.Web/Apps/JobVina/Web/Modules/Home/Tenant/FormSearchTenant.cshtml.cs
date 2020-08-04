19882f9ff99f6d069834ffa483442ee0e75a50e3
Hau Soft <50167033+hauerp@users.noreply.github.com>
2020-08-05 00:16:30 +0700

    Clean Code
    Clean Code

--- 2.Source/2.1.Web/Apps/JobVina/Web/Modules/Home/Tenant/FormSearchTenant.cshtml.cs
+++ 2.Source/2.1.Web/Apps/JobVina/Web/Modules/Home/Tenant/FormSearchTenant.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using AZWeb.Module.Common;
+﻿using AZCore.Extensions;
+using AZWeb.Module.Attributes;
+using AZWeb.Module.Common;
 using JobVina.Common;
 using Microsoft.AspNetCore.Http;
 
@@ -6,13 +8,22 @@ namespace JobVina.Web.Modules.Home.Tenant
 {
     public class FormSearchTenant : PageHome
     {
+        [BindQuery]
+        public string TextSearch { get; set; }
         public FormSearchTenant(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
         {
         }
 
         public IView Get()
         {
-            this.Title = "JobVINA - Thông tin tuyển dụng và đối tác cung ứng lao động uy tín";
+            if (TextSearch.IsNullOrEmpty())
+            {
+                this.Title = "Tìm đối tác cung ứng lao động uy tín";
+            }
+            else
+            {
+                this.Title = "Tìm đối tác cung ứng:" + TextSearch;
+            }
             return View();
         }
     }
